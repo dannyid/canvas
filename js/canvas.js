fc34dd0fb4ce9fca5ac93d1de8bdfab64bdd929a
dannyid <danielshekhtman@gmail.com>
2013-09-02 07:19:29 -0400

    first commit

---
+++ js/canvas.js
@@ -0,0 +1,42 @@
+
+var c = document.getElementById("canvas").getContext("2d");
+
+//c.beginPath();
+//c.moveTo(100,100);
+//c.lineTo(500,500);
+//c.lineTo(100,500);
+//c.closePath();
+//c.stroke();
+
+var i = 0;
+
+function lines() {
+    if (i <= 400) {
+        c.strokeStyle = "black";
+        c.beginPath();
+        c.moveTo(100, 100+i);
+        c.lineTo(500, 100+i);
+        c.stroke();
+        c.beginPath();
+        c.moveTo(100+i, 100);
+        c.lineTo(100+i, 500);
+        c.stroke();
+        i = i + 8;
+        setTimeout(lines,20);
+    } else {
+        i = 0;
+        c.strokeStyle = "white";
+        c.beginPath();
+        c.moveTo(100, 100+i);
+        c.lineTo(500, 100+i);
+        c.stroke();
+        c.beginPath();
+        c.moveTo(100+i, 100);
+        c.lineTo(100+i, 500);
+        c.stroke();
+        i = i + 8;
+        setTimeout(lines,20);
+    };
+};
+
+lines();
